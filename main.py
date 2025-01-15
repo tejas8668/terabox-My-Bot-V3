c4f7713906a6e555a750c0a1cbaf05c8deb5b48d
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-01-15 22:51:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -458,7 +458,7 @@ async def active(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     # Get the port from the environment variable or use default
     port = int(os.environ.get('PORT', 8080))  # Default to port 8080
-    webhook_url = f"https://accurate-cordula-imdb07-87daeb39.koyeb.app/{TOKEN}"  # Replace with your server URL
+    webhook_url = f"https://painful-eustacia-chavan-013550df.koyeb.app/{TOKEN}"  # Replace with your server URL
 
     # Create the Application and pass it your bot's token
     app = ApplicationBuilder().token(TOKEN).build()
